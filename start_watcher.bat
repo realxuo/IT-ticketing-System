9dd0ffc69be6dd983b683aecc4e62bdbfe91948e
realxuo <antrbrown101@gmail.com>
2026-06-18 11:55:57 +0100

    Update

--- start_watcher.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo.
-echo  Starting auto-push watcher...
-echo  Changes will be pushed to GitHub automatically.
-echo  Close this window to stop.
-echo.
-python watch_and_push.py
-pause
